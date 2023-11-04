fe3788b17ac5b372fb1e3953b7cfc259ea507d93
333Kay333 <140438505+333Kay333@users.noreply.github.com>
2023-11-04 08:55:10 -0400

    #basic

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const App = () => {
+
+  const options = [
+ 
+    { label: 'Knowledge', value: 'Azure, Docker, Puppet, GitLab, Git, Stack, HTML, CSS, JavaScript, React, Nodes.js, SQL ' },
+ 
+    { label: 'Experience', value: 'I have spent the past year educating myself in web development though SheCodes Academy as well as utilizing self learning applications.' },
+ 
+    { label: 'About Me', value: 'I am a single mother.  I am an introvert who loves to work and be at home. '
+    },
+ 
+  ];
+ 
